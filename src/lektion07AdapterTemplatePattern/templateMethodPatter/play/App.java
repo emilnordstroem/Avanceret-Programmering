abb51cd7a2f46dfc694a4f07ea8331dcf458ab52
emilnordstroem <emilstoeve@outlook.com>
2025-11-06 11:59:40 +0100

    Completion of task 3 of lecture 7

--- src/lektion07AdapterTemplatePattern/templateMethodPatter/play/App.java
+++ src/lektion07AdapterTemplatePattern/templateMethodPatter/play/App.java
@@ -5,7 +5,8 @@ public class App {
         // Oprettelse af spørgsmål
         Question[] questions = {
                 new MultipleChoiceQuestion("Hvad er hovedstaden i Frankrig?", new String[]{"Paris", "Berlin", "London"}, 0),
-                new TrueFalseQuestion("Er jorden rund?", true)
+                new TrueFalseQuestion("Er jorden rund?", true),
+                new OpenAnswerQuestion("Hvordan har du det med Template Method Design Pattern?")
         };
 
         // Oprettelse af spil og afvikling
---
+++ src/lektion07AdapterTemplatePattern/templateMethodPatter/play/OpenAnswerQuestion.java
@@ -0,0 +1,29 @@
+package lektion07AdapterTemplatePattern.templateMethodPatter.play;
+
+import java.util.Scanner;
+
+public class OpenAnswerQuestion extends Question {
+    private String question;
+
+    public OpenAnswerQuestion(String question) {
+        this.question = question;
+    }
+
+    @Override
+    protected void displayQuestion() {
+        System.out.println("Open Answer: " + question);
+    }
+
+    @Override
+    protected String getUserAnswer() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Svar på under 280 tegn: ");
+        String answer = scan.nextLine();
+        return answer;
+    }
+
+    @Override
+    protected boolean checkAnswer(String answer) {
+        return true;
+    }
+}
